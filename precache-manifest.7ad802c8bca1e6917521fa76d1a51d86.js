2097ebc9b367cd30e13a3a581c062e142cb90c19
lawal samuel <samthedonz@gmail.com>
2019-12-03 19:39:49 -0800

    Updates

---
+++ precache-manifest.7ad802c8bca1e6917521fa76d1a51d86.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b023e45ecc28ac6a0ff562bd6a8e1c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "10191aaa4bc64c41ba2c",
+    "url": "/portfolio/static/css/main.6528ab2d.chunk.css"
+  },
+  {
+    "revision": "c37be22bbe3c30b52428",
+    "url": "/portfolio/static/js/2.114166de.chunk.js"
+  },
+  {
+    "revision": "10191aaa4bc64c41ba2c",
+    "url": "/portfolio/static/js/main.a5d81f38.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "a5fe3b0383ae21695dc8dd8868ca18c0",
+    "url": "/portfolio/static/media/angular.a5fe3b03.svg"
+  },
+  {
+    "revision": "00072bafbf0e2af0df7738196a30b0c9",
+    "url": "/portfolio/static/media/check-box-empty.00072baf.svg"
+  },
+  {
+    "revision": "0d72d2fc3a2a21662d3799bb98897948",
+    "url": "/portfolio/static/media/css3.0d72d2fc.svg"
+  },
+  {
+    "revision": "0827c5377e148c6f6e7a80b16af19ea9",
+    "url": "/portfolio/static/media/dart.0827c537.svg"
+  },
+  {
+    "revision": "722d5e83aa647073061d9d2036fa06de",
+    "url": "/portfolio/static/media/firebase.722d5e83.svg"
+  },
+  {
+    "revision": "96fc0db8a8ae9019366254487bd5aee5",
+    "url": "/portfolio/static/media/flutter.96fc0db8.svg"
+  },
+  {
+    "revision": "4125e356e6d9c29652fb912edfa4e35b",
+    "url": "/portfolio/static/media/html5.4125e356.svg"
+  },
+  {
+    "revision": "a25ef0280bff53d3eb832ef7954401a6",
+    "url": "/portfolio/static/media/ionic.a25ef028.svg"
+  },
+  {
+    "revision": "f1f848233b0981c98a2d247a96398377",
+    "url": "/portfolio/static/media/javascript.f1f84823.svg"
+  },
+  {
+    "revision": "7de0beb40e91690398dd4efcc77b5d65",
+    "url": "/portfolio/static/media/new3.7de0beb4.png"
+  },
+  {
+    "revision": "14bfa80979c87949d798858e86cc0424",
+    "url": "/portfolio/static/media/nodejs.14bfa809.svg"
+  },
+  {
+    "revision": "2fe7d83ce1f727f1c4e072f6c9eff0df",
+    "url": "/portfolio/static/media/react.2fe7d83c.svg"
+  }
+]);
